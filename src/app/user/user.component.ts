da1154e16cfe2fae19fd0ebeb8476a120b343688
sinvalfelisberto <sinvalfelisberto@gmail.com>
2025-03-06 22:56:25 -0300

    learning about how angular works...

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -7,5 +11,20 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
+
+  // get imagePath() {
+  //   return 'assets/users/' + this.selectedUser().avatar;
+  // }
+
+  get userName() {
+    return this.selectedUser().name;
+  }
+
+  onSelectUser() {
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  }
 
 }
